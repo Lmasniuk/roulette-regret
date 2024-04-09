6be6bf20d2151f3dff98e57c589c87d03582acf1
Logan Masniuk <logan.masniuk@gmail.com>
2024-04-09 11:51:45 -0700

    Add some basic styling(colors, outcome box borders)

--- src/components/GamesOutcome/GamesOutcome.jsx
+++ src/components/GamesOutcome/GamesOutcome.jsx
@@ -1,11 +1,13 @@
+import './GamesOutcome.scss'
+
 function GamesOutcome({gamesResult}){
 
 
     return (
 <div class="games-outcome">
-    <h3>Total Earnings: ${gamesResult.totalEarnings}</h3>
-    <h3>Total Spins Where a Number was Hit: {gamesResult.wins}</h3>
-    <h3>Total Spins Where a Number was Not Hit:  {gamesResult.losses}</h3>
+    <h3>Total Earnings: <span className={gamesResult.totalEarnings >0 ? "positive-earnings" : "negative-earnings"}>${gamesResult.totalEarnings}</span></h3>
+    <h4>Total Spins Where a Number was Hit: {gamesResult.wins}</h4>
+    <h4>Total Spins Where a Number was Not Hit:  {gamesResult.losses}</h4>
   </div>
     )
     
